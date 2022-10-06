ad0f6fd85eea709a8493d618be075a3404508577
omnipede <omnipede@naver.com>
2022-10-06 10:16:01 +0900

    기능: DataLoader 수정
    서벗 실행 시 product 데이터를 저장하도록 수정함

--- src/main/java/io/omnipede/boilerplate/system/component/DataLoader.java
+++ src/main/java/io/omnipede/boilerplate/system/component/DataLoader.java
@@ -2,46 +2,47 @@ package io.omnipede.boilerplate.system.component;
 
 import io.omnipede.boilerplate.domain.product.Product;
 import io.omnipede.boilerplate.domain.product.ProductRepository;
-import io.omnipede.boilerplate.domain.user.User;
-import io.omnipede.boilerplate.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+/**
+ * 서버 실행 시 데모 데이터를 로딩하기 위한 컴포넌트
+ */
 @Slf4j
 @Component
 @RequiredArgsConstructor
 class DataLoader implements ApplicationRunner {
 
     private final ProductRepository productRepository;
-    private final UserRepository userRepository;
 
     @Override
     public void run(ApplicationArguments args) {
 
-        // TODO save if not exists
-//        log.info("Loading default products...");
-//        Product toy = Product.builder()
-//                .name("Monitor")
-//                .price(100000L)
-//                .build();
-//
-//        Product food = Product.builder()
-//                .name("HDD")
-//                .price(50000L)
-//                .build();
-//
-//        productRepository.save(toy);
-//        productRepository.save(food);
-//
-//        log.info("Loading default users...");
-//        User user = User.builder()
-//                .email("omnipede@naver.com")
-//                .password("password")
-//                .build();
-//
-//        userRepository.save(user);
+        // Save demo data if DB is empty
+        if (productRepository.count() > 0)
+            return;
+
+        log.info("Loading default products...");
+        Product monitor = Product.builder()
+                .name("Monitor")
+                .price(100000L)
+                .build();
+
+        Product hdd = Product.builder()
+                .name("HDD")
+                .price(50000L)
+                .build();
+
+        Product ssd = Product.builder()
+                .name("SSD")
+                .price(80000L)
+                .build();
+
+        productRepository.save(monitor);
+        productRepository.save(hdd);
+        productRepository.save(ssd);
     }
 }
